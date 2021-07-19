0d1c573e5ed0c53c3d66686d4afeac06769d363e
mariamalshammari <malshammari37@gmail.com>
2021-07-19 18:46:58 +0300

    lab14BackEndDone

--- server.js
+++ server.js
@@ -15,31 +15,33 @@ const PORT = process.env.PORT;
 mongoose.connect('mongodb://localhost:27017/books', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const booksSchema = new mongoose.Schema({
-    name:String,
-    description:String,
-    status:String,
-    img:String
+    name: String,
+    description: String,
+    status: String,
+    img: String
 
 });
 
 const UserSchema = new mongoose.Schema({
     // name:String,
 
-    email:String,
-    books:[booksSchema]
+    email: String,
+    books: [booksSchema]
 
 
 });
 
-const myBookModel=mongoose.model('book',booksSchema);
-const userModel=mongoose.model('owner',UserSchema);
+const myBookModel = mongoose.model('book', booksSchema);
+const userModel = mongoose.model('owner', UserSchema);
 
-function seadOwnerCollection(){
-    const mariam = new userModel({email: 'malshammari37@gmail.com', books: [
-        { name: 'The Growth Mindset', description: 'Dweck coined the terms fixed mindset and growth mindset to describe the underlying beliefs people have about learning and intelligence. When students believe they can get smarter, they understand that effort makes them stronger. Therefore they put in extra time and effort, and that leads to higher achievement.', status: 'FAVORITE FIVE', img: 'https://m.media-amazon.com/images/I/61bDwfLudLL._AC_UL640_QL65_.jpg' },
-        { name: 'The Momnt of Lift', description: 'Melinda Gates shares her how her exposure to the poor around the world has established the objectives of her foundation.', status: 'RECOMMENDED TO ME', img: 'https://m.media-amazon.com/images/I/71LESEKiazL._AC_UY436_QL65_.jpg'}
-      ]})
-      mariam.save();
+function seadOwnerCollection() {
+    const mariam = new userModel({
+        email: 'malshammari37@gmail.com', books: [
+            { name: 'The Growth Mindset', description: 'Dweck coined the terms fixed mindset and growth mindset to describe the underlying beliefs people have about learning and intelligence. When students believe they can get smarter, they understand that effort makes them stronger. Therefore they put in extra time and effort, and that leads to higher achievement.', status: 'FAVORITE FIVE', img: 'https://m.media-amazon.com/images/I/61bDwfLudLL._AC_UL640_QL65_.jpg' },
+            { name: 'The Momnt of Lift', description: 'Melinda Gates shares her how her exposure to the poor around the world has established the objectives of her foundation.', status: 'RECOMMENDED TO ME', img: 'https://m.media-amazon.com/images/I/71LESEKiazL._AC_UY436_QL65_.jpg' }
+        ]
+    })
+    mariam.save();
 }
 
 seadOwnerCollection();
@@ -59,38 +61,39 @@ function homePageHandler(req, res) {
 app.get('/books', booksHandler);
 
 function booksHandler(req, res) {
-let userEmail=req.query.userEmail;
-userModel.find(
-    {email:userEmail},function(error,ownerData){
-    if(error){
-        res.send(error,'did not work')
-    }
-    else{
-        res.send(ownerData[0].books)
-    }
-    
-})
+    let userEmail = req.query.userEmail;
+    userModel.find(
+        { email: userEmail }, function (error, ownerData) {
+            if (error) {
+                res.send(error, 'did not work')
+            }
+            else {
+                res.send(ownerData[0].books)
+            }
+
+        })
 
 }
 
 // http://localhost:3005/addBook?name=book1&description=about1&status=readed&userEmail=malshammari37@gmail.com
-app.post('/addBook',addBookHandler);
+app.post('/addBook', addBookHandler);
 
 // localhost:3005/books/1?userEmail=malshammari37@gmail.com
-app.delete('/books/:bookId',deleteBookHandler)
+app.delete('/books/:bookId', deleteBookHandler)
+app.put('/updateBook/:bookId', updateBookHandler)
 
 
-function addBookHandler(req,res) {
+function addBookHandler(req, res) {
     console.log(req.body)
-    let {name,status,description,img,Email}= req.body
+    let { name, status, description, img, Email } = req.body
     console.log(Email);
 
-    userModel.find({ email: Email},(error,booksData) => {
+    userModel.find({ email: Email }, (error, booksData) => {
         if (error) {
-            res.send(error,'wrong user')
+            res.send(error, 'wrong user')
         }
         else {
-            console.log('befor',booksData[0])
+            console.log('befor', booksData[0])
             booksData[0].books.push({
                 name: name,
                 description: description,
@@ -107,36 +110,63 @@ function addBookHandler(req,res) {
 
         }
     })
-    
+
 
 }
-function deleteBookHandler(req,res) {
-    
-        console.log(req.params)
-        console.log(req.query)
-    
-        let bookId=Number(req.params.bookId)
-        console.log(bookId);
-        let userEmail=req.query.userEmail
-        console.log(userEmail)
-        userModel.find({email:userEmail},(error,booksData)=>{
-    
-            if(error){res.send(error,'not delete')}
-            else{
-            let newBookArr=booksData[0].books.filter((item,idx)=>{
+function deleteBookHandler(req, res) {
+
+    console.log(req.params)
+    console.log(req.query)
+
+    let bookId = Number(req.params.bookId)
+    console.log(bookId);
+    let userEmail = req.query.userEmail
+    console.log(userEmail)
+    userModel.find({ email: userEmail }, (error, booksData) => {
+
+        if (error) { res.send(error, 'not delete') }
+        else {
+            let newBookArr = booksData[0].books.filter((item, idx) => {
                 // return idx !==bookId
-                if(idx !== bookId) 
-                return item
-    
+                if (idx !== bookId)
+                    return item
+
             })
-            booksData[0].books=newBookArr
-            console.log('data after del',booksData[0].books)
+            booksData[0].books = newBookArr
+            console.log('data after del', booksData[0].books)
             booksData[0].save()
             res.send(booksData[0].books)
         }
-    
-        })
-    }
+
+    })
+}
+
+function updateBookHandler(req, res) {
+    console.log('params:', req.params)
+    console.log('query:', req.query)
+    let { name, status, description, img, userEmail } = req.body
+    console.log(userEmail);
+    let bookId = Number(req.params.bookId);
+    console.log(bookId);
+    userModel.findOne({ email: userEmail }, (error, booksData) => {
+
+        if (error) { res.send(error, 'not delete') }
+        else {
+            console.log(booksData)
+            booksData.books.splice(bookId, 1, {
+                name: name,
+                status: status,
+                description: description,
+                img: img
+            })
+            console.log(booksData)
+            booksData.save();
+            res.send(booksData.books)
+
+        }
+    })
+
+}
 
 
 
